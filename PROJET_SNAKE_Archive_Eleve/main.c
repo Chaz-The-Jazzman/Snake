efb24d80932ebb9907ae166647f5e2f1ff1c20b2
Chaz-The-Jazzman <c.holme2112@gmail.com>
2024-12-16 10:42:11 +0100

    Add files via upload
    Starting template

---
+++ PROJET_SNAKE_Archive_Eleve/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "myLib.h"
+#include "SP_GESTION_JEU.h"
+
+int main()
+{
+
+
+    setColor(RED) ;
+
+    printf("Bienvenue dans le SNAKE!!!\n");
+
+    setColor (GREEN) ;
+    printf(" A vous de jouer !!!\n");
+
+    return 0;
+}
